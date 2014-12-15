044135847bb28375f84ab3d69ff2d4ef407b8138
Alberto-Vivar <killer7129@gmail.com>
2014-12-15 18:56:21 +0100

    Model de datos de la aplicación
    Modelo de los datos de la aplicación y su respectiva interfaz.

--- src/ubu/lsi/dms/agenda/modelo/Llamada.java
+++ src/ubu/lsi/dms/agenda/modelo/Llamada.java
@@ -1,20 +1,20 @@
 package ubu.lsi.dms.agenda.modelo;
 
 import java.io.Serializable;
+
 /**
  * Clase de entidad con la información de Llamada
+ * 
  * @author Carlos López
  *
  */
 @SuppressWarnings("serial")
-public class Llamada implements Serializable{
-	
-	 private int idLlamada;
-	 private Contacto contacto;
-	 private String fechaLlamada ;
-	 private String asunto,notas;
-	 
-	 
+public class Llamada implements Serializable {
+
+	private int idLlamada;
+	private Contacto contacto;
+	private String fechaLlamada;
+	private String asunto, notas;
 
 	public Llamada(int idLlamada, Contacto contacto, String fechaLlamada,
 			String asunto, String notas) {
@@ -25,17 +25,111 @@ public class Llamada implements Serializable{
 		this.asunto = asunto;
 		this.notas = notas;
 	}
-	
-	@Override
-	public String toString() {
-		return "Llamada [idLlamada=" + idLlamada + ", contacto=" + contacto
-				+ ", fechaLlamada=" + fechaLlamada + ", asunto=" + asunto
-				+ ", notas=" + notas + "]";
+
+	/**
+	 * @return the contacto
+	 */
+	public Contacto getContacto() {
+		return contacto;
 	}
 
-	 
-	 
-	   
-	
+	/**
+	 * @param contacto
+	 *            the contacto to set
+	 */
+	public void setContacto(Contacto contacto) {
+		this.contacto = contacto;
+	}
+
+	/**
+	 * @return the fechaLlamada
+	 */
+	public String getFechaLlamada() {
+		return fechaLlamada;
+	}
+
+	/**
+	 * @param fechaLlamada
+	 *            the fechaLlamada to set
+	 */
+	public void setFechaLlamada(String fechaLlamada) {
+		this.fechaLlamada = fechaLlamada;
+	}
+
+	/**
+	 * @return the asunto
+	 */
+	public String getAsunto() {
+		return asunto;
+	}
+
+	/**
+	 * @param asunto
+	 *            the asunto to set
+	 */
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	/**
+	 * @return the notas
+	 */
+	public String getNotas() {
+		return notas;
+	}
+
+	/**
+	 * @param notas
+	 *            the notas to set
+	 */
+	public void setNotas(String notas) {
+		this.notas = notas;
+	}
+
+	/**
+	 * @return the idLlamada
+	 */
+	public int getIdLlamada() {
+		return idLlamada;
+	}
+
+	@Override
+	public String toString() {
+		return contacto.getApellidos() + ", " + contacto.getNombre();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Llamada other = (Llamada) obj;
+		if (asunto == null) {
+			if (other.asunto != null)
+				return false;
+		} else if (!asunto.equals(other.asunto))
+			return false;
+		if (contacto == null) {
+			if (other.contacto != null)
+				return false;
+		} else if (!contacto.equals(other.contacto))
+			return false;
+		if (fechaLlamada == null) {
+			if (other.fechaLlamada != null)
+				return false;
+		} else if (!fechaLlamada.equals(other.fechaLlamada))
+			return false;
+		if (idLlamada != other.idLlamada)
+			return false;
+		if (notas == null) {
+			if (other.notas != null)
+				return false;
+		} else if (!notas.equals(other.notas))
+			return false;
+		return true;
+	}
 
 }
